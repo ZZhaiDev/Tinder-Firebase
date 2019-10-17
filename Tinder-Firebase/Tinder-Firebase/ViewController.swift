c344e1f98e27b3b557fa7734c3900fd05a58f3f8
zijiazhai <zijiazhai@gmail.com>
2019-10-17 16:20:08 -0500

    pan gesture

--- Tinder-Firebase/Tinder-Firebase/ViewController.swift
+++ Tinder-Firebase/Tinder-Firebase/ViewController.swift
@@ -10,19 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let topStackVIew = TopNavigationStackView()
-    let blueView = UIView()
-    let yellowView = HomeBottomControlsStackView()
+    let topStackView = TopNavigationStackView()
+    let cardsDeckView = UIView()
+    let buttonsStackView = HomeBottomControlsStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        blueView.backgroundColor = .blue
-        
-        let overallStackView = UIStackView(arrangedSubviews: [topStackVIew, blueView, yellowView])
+        setupLayout()
+        setupDummyCards()
+    }
+    
+    fileprivate func setupDummyCards() {
+        print("Setting up dummy cards")
+        let cardView = CardView(frame: .zero)
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
+    
+    // MARK:- Fileprivate
+    
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
         overallStackView.axis = .vertical
         view.addSubview(overallStackView)
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
 
 
---
+++ Tinder-Firebase/Tinder-Firebase/Views/CardView.swift
@@ -0,0 +1,58 @@
+//
+//  CardView.swift
+//  Tinder-Firebase
+//
+//  Created by zijia on 10/17/19.
+//  Copyright © 2019 zijia. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        let panGuesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(gesture:)))
+        addGestureRecognizer(panGuesture)
+       
+    }
+    
+    @objc private func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            break
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }) { (_) in
+            
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
