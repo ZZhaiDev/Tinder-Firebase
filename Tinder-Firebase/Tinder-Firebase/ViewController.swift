b6ce8248cfd087669e54c15d33e557bf7274ea45
zijiazhai <zijiazhai@gmail.com>
2019-10-17 14:11:13 -0500

    iniit

---
+++ Tinder-Firebase/Tinder-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder-Firebase
+//
+//  Created by zijia on 10/17/19.
+//  Copyright © 2019 zijia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
