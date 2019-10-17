5698f23eb860b3dc4587ec6782205d2492064be9
zijiazhai <zijiazhai@gmail.com>
2019-10-17 14:21:13 -0500

    basic stackView

--- Tinder-Firebase/Tinder-Firebase/ViewController.swift
+++ Tinder-Firebase/Tinder-Firebase/ViewController.swift
@@ -12,7 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let subVIews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackVIew = UIStackView(arrangedSubviews: subVIews)
+        topStackVIew.distribution = .fillEqually
+        topStackVIew.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackVIew, blueView, yellowView])
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        stackView.fillSuperview()
     }
 
 
