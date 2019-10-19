d007b906cd5f9f10556d3b24db392d035aa939d5
zijiazhai <zijiazhai@gmail.com>
2019-10-19 13:46:48 -0500

    Registration, and keyboard

---
+++ Tinder-Firebase/Tinder-Firebase/RegistrationController.swift
@@ -0,0 +1,133 @@
+//
+//  RegistrationController.swift
+//  Tinder-Firebase
+//
+//  Created by zijia on 10/19/19.
+//  Copyright © 2019 zijia. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // UI Components
+    let selectPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Select Photo", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    let fullNameTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 24, height: 44)
+        tf.placeholder = "Enter full name"
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    let emailTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 24, height: 44)
+        tf.placeholder = "Enter email"
+        tf.keyboardType = .emailAddress
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    let passwordTextField: RegistrationTextField = {
+        let tf = RegistrationTextField(padding: 24, height: 44)
+        tf.placeholder = "Enter password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = .white
+        return tf
+    }()
+    
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
+        button.backgroundColor = #colorLiteral(red: 0.8235294118, green: 0, blue: 0.3254901961, alpha: 1)
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.layer.cornerRadius = 22
+        return button
+    }()
+    
+    var stackView = UIStackView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGradientLayer()
+        setupLayout()
+        setupNotificationObservers()
+        setupTapGesture()
+        
+    }
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.view.endEditing(true) // dismisses keyboard
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc fileprivate func handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        })
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        // how to figure out how tall the keyboard actually is
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrame = value.cgRectValue
+        
+        // let's try to figure out how tall the gap is from the register button to the bottom of the screen
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        
+        let difference = keyboardFrame.height - bottomSpace
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self) // you'll have a retain cycle
+    }
+    
+    fileprivate func setupLayout() {
+        stackView = UIStackView(arrangedSubviews: [
+            selectPhotoButton,
+            fullNameTextField,
+            emailTextField,
+            passwordTextField,
+            registerButton
+            ])
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
+        // make sure to user cgColor
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.bounds
+    }
+    
+}
---
+++ Tinder-Firebase/Tinder-Firebase/Views/RegistrationTextField.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  Tinder-Firebase
+//
+//  Created by zijia on 10/19/19.
+//  Copyright © 2019 zijia. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTextField: UITextField {
+    
+    let padding: CGFloat
+    let height: CGFloat
+    
+    init(padding: CGFloat, height: CGFloat) {
+        self.padding = padding
+        self.height = height
+        super.init(frame: .zero)
+        layer.cornerRadius = height / 2
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: height)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
